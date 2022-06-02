f5497033029d744cba545c4681a67cac5dee8ef6
Mulangu Mundemba <mulanguk.mundemba@gmail.com>
2022-06-02 10:28:40 -0500

    Create the main body of the app, ready to add functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/screens/tasks_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,6 +8,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: TasksScreen(),
+    );
   }
 }
